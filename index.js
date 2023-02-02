ba29330efb3c3aeb933afad3500323bd1a6cdf04
Kaique Bernardo <kaique@MacBook-Kaique.local>
2023-02-02 18:42:01 -0300

    api template

--- index.js
+++ index.js
@@ -9,24 +9,47 @@ server.use(express.json())
 
 const cursos = ["Node", "JavaScript","React"]
 
+const checkCursos = (req, res, next) => {
+  if (!req?.body?.name) {
+    return res.status(400).json({ error: "Nome obrigatório" });
+  }
+  return next();
+};
+
+const checkIndexCurso = (req, res, next) => {
+    
+    const curso= cursos[req.params.index]
+    if (!curso) {
+      return res.status(400).json({ error: "O curso não existe" });
+    }
+    return next();
+  };
+
+server.use((req,res,next)=>{
+    
+    console.log(`Url:${req.url}`);
+    return next();
+})
+
 server.get('/cursos', (req,res)=>{
        
         return res.json(cursos)
         
     })
 
-server.get('/curso/:index', (req,res)=>{
+server.get('/curso/:index',checkIndexCurso, (req,res)=>{
 const {index} = req.params;
     return res.json(cursos[index])
 })
 
-server.post('/cursos', (req,res)=>{
+server.post('/cursos',checkCursos, (req,res)=>{
     const {name} = req.body;
+    
     cursos.push(name);
         return res.json(cursos)
     })
 
-server.put('/cursos/:index', (req,res)=>{
+server.put('/cursos/:index',checkCursos,checkIndexCurso, (req,res)=>{
     const {index} = req.params;
     const {name} = req.body;
     
@@ -35,7 +58,7 @@ server.put('/cursos/:index', (req,res)=>{
         return res.json(cursos)
 })
 
-server.delete('/cursos/:index', (req,res)=>{
+server.delete('/cursos/:index',checkIndexCurso, (req,res)=>{
     const {index} = req.params;
         
     cursos.splice(index,1)
