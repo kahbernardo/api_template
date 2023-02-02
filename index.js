5e98d980fd0c75eec9919e24f8a7316107d44b8d
Kaique Bernardo <kaique@MacBook-Kaique.local>
2023-02-02 18:17:07 -0300

    api examples request

--- index.js
+++ index.js
@@ -2,14 +2,40 @@ const express = require('express')
 
 const server = express()
 
+server.use(express.json())
+//query params = ?param=param
+//route params = /param/2
+//Request Body = {a:a, b:b}
+
 const cursos = ["Node", "JavaScript","React"]
+
+server.get('/cursos', (req,res)=>{
+       
+        return res.json(cursos)
+        
+    })
 
 server.get('/curso/:index', (req,res)=>{
 const {index} = req.params;
-
     return res.json(cursos[index])
-    
 })
 
+server.post('/cursos', (req,res)=>{
+    const {name} = req.body;
+    cursos.push(name);
+        return res.json(cursos)
+    })
+
+server.put('/cursos/:index', (req,res)=>{
+    const {index} = req.params;
+    const {name} = req.body;
+    
+    cursos[index]=name;
+    
+        return res.json(cursos)
+})
+    
+
+    
 
 server.listen(3000)
